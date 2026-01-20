5356f8bb744db022359a44d4e7c0a044f81e7a78
davidsantana1 <davidsantanar01@gmail.com>
2026-01-20 17:53:46 -0400

    Initialize FastAPI project with database setup, models, and authentication routes. Added .gitignore, requirements, and schemas for user and todo management.

---
+++ routers/auth.py
@@ -0,0 +1,74 @@
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, status
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from models import Users
+from schemas import CreateUserRequest
+from fastapi.security import OAuth2PasswordRequestForm
+
+router = APIRouter()
+
+SECRET_KEY = "fc9b97100746affb0cbd9b30c7b68898e32b621b2d93d01b1a11f3ae1ee3cfdb"
+ALGORITH = "HS256"
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def authenticate_user(username: str, password: str, db: db_dependency):
+    user = db.query(Users).filter(Users.username == username).first()
+
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    user_from_db = (
+        db.query(Users).filter(Users.email == create_user_request.email).first()
+    )
+
+    if user_from_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="An user with this email already exists.",
+        )
+
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True,
+    )
+
+    db.add(create_user_model)
+    db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        return "Failed Authentication"
+    return "Successful Authentication"
